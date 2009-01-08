45ee50201bef0c8f64c55c8419f05da106b074fa
Pat Maddox <pat.maddox@gmail.com>
2009-01-08 13:46:14 -0800

    Initial commit to has_components.

---
+++ spec/has_components_spec.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe "HasComponents" do
+  it "fails" do
+    raise "hey buddy, you should probably rename this file and start specing for real"
+  end
+end
