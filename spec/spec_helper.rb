944d979ccf6c28ddc023574ef965a31838c3a2ca
Pat Maddox <pat.maddox@gmail.com>
2009-01-08 14:03:17 -0800

    use db

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
 require 'rubygems'
 require 'spec'
+require 'active_record'
+this_dir = File.expand_path(File.dirname(__FILE__))
+require this_dir + '/../lib/acts_as_value_object.rb'
+
+FileUtils.mkdir(this_dir + '/log') unless File.directory?(this_dir + '/log')
+ActiveRecord::Base.logger = Logger.new(this_dir + "/log/test.log")
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => "#{this_dir}/db/test.sqlite3")
+load(this_dir + '/db/schema.rb')
